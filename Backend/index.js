f290390ed2233ff0414b8f323fcc09bf86f21b3d
Sayanduary <sayanduary@gmail.com>
2025-04-04 20:30:25 +0530

    MongoDB Connection

---
+++ Backend/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config({path:'./env'});
+import dotenv from 'dotenv'
+import { app } from './app.js';
+import { connectDB } from './db/index.js';
+
+dotenv.config({
+  path: './.env'
+})
+
+
+
+connectDB()
+  .then(() => {
+
+    app.on('Error', (error) => {
+      console.log('Error', error)
+      throw error;
+
+    })
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at Port : ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log("MongoDB  Connection failed", error);
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
