ed2af817acd8d096ef8b860cd57035ebf9866146
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-05 13:13:19 +0530

    adding and deleting product in myList

--- Backend/routes/mylist.route.js
+++ Backend/routes/mylist.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { addToMyListController } from "../controllers/mylist.controller.js";
+import { addToMyListController, deleteToMyListController, getMyListController } from "../controllers/mylist.controller.js";
 
 const myListRouter = Router();
 
 myListRouter.post('/add',auth,addToMyListController)
+myListRouter.get('/',auth,getMyListController)
+myListRouter.delete('/:id',auth,deleteToMyListController)
+
 
 export default myListRouter;
