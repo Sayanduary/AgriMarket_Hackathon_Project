03c233fa8e31a45cc6265651b4883b8d78d5277b
Sayanduary <sayanduary@gmail.com>
2025-04-05 23:27:25 +0530

    Product Model Setup

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -1,1 +1,26 @@
-//needs to add from video
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  images: [
+    {
+      type: String,
+      required: true,
+    }
+  ],
+  brand: {
+    type: String,
+    default: '',
+  },
+  price: {
+    type: Number,
+
+  }
+}, { timestamps: true })
