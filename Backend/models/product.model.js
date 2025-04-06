79638cc8d872b303d72b837251f47fd1b09735eb
Sayanduary <sayanduary@gmail.com>
2025-04-06 11:21:52 +0530

    productModelCreated

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
+import { type } from 'os'
 
 const productSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,101 @@ const productSchema = new mongoose.Schema({
   },
   price: {
     type: Number,
+    default: 0,
+  },
+  oldPrice: {
+    type: Number,
+    defaut: 0,
+  },
+  catName: {
+    type: String,
+    default: ''
+  },
+  catId: {
+    type: String,
+    default: '',
+  },
+  subCatId: {
+    type: String,
+    default: '',
 
+  },
+  subCat: {
+    type: String,
+    default: '',
+  },
+  thirdsubCat: {
+    type: String,
+    default: '',
+  },
+  thirdsubCatId: {
+    type: String,
+    default: '',
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+
+  discount: {
+    type: Number,
+    required: true,
+  },
+
+  productRam: [
+    {
+      type: String,
+      default: null,
+    }
+  ]
+  ,
+  size: [
+    {
+      type: String,
+      default: null,
+    }
+  ]
+  ,
+  productHeight: [
+    {
+      type: String,
+      default: null,
+    }
+  ]
+  ,
+
+  productWeight: [
+    {
+      type: String,
+      default: null,
+    }
+  ],
+
+  dateCreated: {
+    type: String,
+    default: Date.now,
   }
+  ,
+
+
+
 }, { timestamps: true })
+
+
+const productModel = mongoose.model('Product', productSchema);
+
+export default productModel;
